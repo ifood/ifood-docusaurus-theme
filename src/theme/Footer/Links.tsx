57f3109b4b1d2e91fce200197700bb8dcdeee5d5
Adriel Albuquerque Cavalcanti <adriel.cavalcanti@ifood.com.br>
2021-11-25 18:04:11 -0300

    feat(styles): Adds better-bem

--- src/theme/Footer/Links.tsx
+++ src/theme/Footer/Links.tsx
@@ -1,20 +1,21 @@
 import { Link } from "@docusaurus/router";
 import * as React from "react";
+import bem from 'better-bem';
 
 import { LinksType } from "./types";
 
-export function Links({links, idx }: LinksType) {
-  return (
-    <div className="col col--3" key={`footer-link-${idx}`}>
-      {links.title && <h4>{links.title}</h4>}
-      <div className="footer__links">
-        {links.items?.map((item) => (
-          <div className="footer__link">
-            {item.href && <a className="footer__link" href={item.href}>{item.label}</a>}
-            {item.to && <Link className="footer__link" to={item.to}>{item.label}</Link>}
-          </div>
-        ))}
-      </div>
+const { el } = bem('footer')
+
+export const Links = ({links, idx }: LinksType) => (
+  <div className="col col--3" key={`footer-link-${idx}`}>
+    {links.title && <h4>{links.title}</h4>}
+    <div className={el('links').cn}>
+      {links.items?.map((item) => (
+        <div className={el('link').cn}>
+          {item.href && <a className={el('link').cn} href={item.href}>{item.label}</a>}
+          {item.to && <Link className={el('link').cn} to={item.to}>{item.label}</Link>}
+        </div>
+      ))}
     </div>
-  );
-}
+  </div>
+);
