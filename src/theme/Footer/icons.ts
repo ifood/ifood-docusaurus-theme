4baf7877fea18fc53055c3b9d7f15db7ac405eb0
Italo Sousa <italo.sousa@ifood.com.br>
2021-11-25 11:26:51 -0300

    refactor(icons): Use icons as svg

---
+++ src/theme/Footer/icons.ts
@@ -0,0 +1,5 @@
+export const Facebook = require("../../assets/icons/icon-facebook.svg").default;
+export const Instagram = require("../../assets/icons/icon-instagram.svg").default;
+export const Twitter = require("../../assets/icons/icon-twitter.svg").default;
+export const Youtube = require("../../assets/icons/icon-youtube.svg").default;
+export const Logo = require("../../assets/logo.svg").default;
